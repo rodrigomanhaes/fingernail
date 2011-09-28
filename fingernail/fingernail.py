f7c60ebfb40b95cf5c708affd03448b836951b12
Rodrigo Manhães <rmanhaes@gmail.com>
2011-09-27 23:40:03 -0300

    added support to negative expectations

--- fingernail/fingernail.py
+++ fingernail/fingernail.py
@@ -7,12 +7,20 @@ class expect(object):
         self.actual = actual
 
     def __getattr__(self, attrname):
-        r = re.match(r'to_(.+)', attrname)
-        return self._run_matcher(_matchers[r.group(1)])
+        r = re.match(r'to_(not_)?(.+)', attrname)
+        return self._run_matcher(_matchers[r.group(2)], to_not=r.group(1))
 
-    def _run_matcher(self, matcher):
-        if not matcher.match(self.actual):
-            raise BrokenExpectation(matcher.message_for_failed_should())
+    def _run_matcher(self, matcher, to_not):
+        if not self._evaluate(matcher, to_not):
+            raise BrokenExpectation(
+              to_not and matcher.message_for_failed_should_not() \
+                      or matcher.message_for_failed_should())
+
+    def _evaluate(self, matcher, to_not):
+        if to_not:
+            return not matcher.match(self.actual)
+        return matcher.match(self.actual)
+
 
 
 _matchers = {}
